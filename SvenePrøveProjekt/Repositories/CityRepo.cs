6f1dff38b8abcb9c03d401d7980fcf61b53fd5ea
Haittham <hait0029@gmail.com>
2024-06-03 14:55:27 +0200

    some repo

--- SvenePrøveProjekt/Repositories/CityRepo.cs
+++ SvenePrøveProjekt/Repositories/CityRepo.cs
@@ -28,9 +28,17 @@ namespace SvenePrøveProjekt.Repositories
             return await _context.City.FirstOrDefaultAsync(x => x.CityID == cityId);
         }
 
-        public async Task<City> UpdateCity(City city, int cityId)
+        public async Task<City> UpdateCity(City updatecity, int cityId)
         {
-            throw new NotImplementedException();
+            City city = await GetCityById(cityId);
+            if (city != null && UpdateCity != null)
+            {
+                city.CityName = updatecity.CityName;
+                city.CityID = updatecity.CityID;
+
+                await _context.SaveChangesAsync();
+            }
+            return city;
         }
 
         public async Task<City> DeleteCity(int cityId)
