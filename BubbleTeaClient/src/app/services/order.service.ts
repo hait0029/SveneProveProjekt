e59765b3fec38229e01628993d4cc63a71ec7d03
Haittham <hait0029@gmail.com>
2024-08-23 08:45:08 +0200

    services done

--- BubbleTeaClient/src/app/services/order.service.ts
+++ BubbleTeaClient/src/app/services/order.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environments';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Order } from '../models/Order';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor() { }
+  private apiUrl= environment.apiurl + 'ProductList';
+
+  constructor(private http: HttpClient) { }
+
+  getAll():Observable<Order[]>{
+    return this.http.get<Order[]>(this.apiUrl)
+  }
+  getById(orderId:number): Observable<Order>{
+    return this.http.get<Order>(`${this.apiUrl}/${orderId}`);
+  }
+  create(order:Order): Observable<Order>{
+    return this.http.post<Order>(this.apiUrl, order);
+  }
+
+  update(order:Order): Observable<Order>{
+    return this.http.put<Order>(`${this.apiUrl}/${order.orderID}`, order);
+  }
+
+  delete(orderId:number): Observable<Order>{
+    return this.http.delete<Order>(`${this.apiUrl}/${orderId}`);
+  }
 }
--- BubbleTeaClient/src/app/services/user.service.ts
+++ BubbleTeaClient/src/app/services/user.service.ts
@@ -1,9 +1,32 @@
+import { User } from './../models/User';
+import { environment } from '../../environments/environments';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private apiUrl= environment.apiurl + 'User';
+
+  constructor(private http: HttpClient) { }
+
+  getAll():Observable<User[]>{
+    return this.http.get<User[]>(this.apiUrl)
+  }
+  getById(userId:number): Observable<User>{
+    return this.http.get<User>(`${this.apiUrl}/${userId}`);
+  }
+  create(user:User): Observable<User>{
+    return this.http.post<User>(this.apiUrl, user);
+  }
+
+  update(user:User): Observable<User>{
+    return this.http.put<User>(`${this.apiUrl}/${user.userID}`, user);
+  }
+
+  delete(userId:number): Observable<User>{
+    return this.http.delete<User>(`${this.apiUrl}/${userId}`);
+  }
 }
