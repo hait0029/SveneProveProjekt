1c570710ba5beb1ae4365ccc33546bf57a289a2d
Dragoncoder5 <jivamarco@gmail.com>
2024-10-14 09:30:57 +0200

    Login css and html

--- SvenePrøveProjekt/Migrations/20240920104300_Bubble-Web-Api.cs
+++ SvenePrøveProjekt/Migrations/20240920104300_Bubble-Web-Api.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace SvenePrøveProjekt.Migrations
 {
     /// <inheritdoc />
-    public partial class initial : Migration
+    public partial class BubbleWebApi : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
